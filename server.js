fce48b0802da5600b281d46785f61ebc88801360
sasmitha-bit <sasmithaln8@gmail.com>
2026-04-28 20:19:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const cors = require("cors");
 
@@ -7,41 +6,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Temporary storage
 let tasks = [];
 
-// Home route
-app.get("/tasks", (req, res) => {
-res.json(tasks);
+app.get("/", (req, res) => {
+  res.send("Task Manager API Running 🚀");
 });
 
-// CREATE TASK
-app.post("/tasks", (req, res) => {
-  tasks.push(req.body);
-  res.json("{ message":"Task added"});
-});
-
-// VIEW TASKS
 app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-// DELETE TASK
+app.post("/tasks", (req, res) => {
+  tasks.push(req.body);
+  res.json({ message: "Task added" });
+});
+
 app.delete("/tasks/:index", (req, res) => {
   const index = req.params.index;
   tasks.splice(index, 1);
-  res.send("Task deleted");
+  res.json({ message: "Deleted" });
 });
 
-// UPDATE TASK (Toggle completed)
 app.put("/tasks/:index", (req, res) => {
   const index = req.params.index;
   tasks[index].completed = !tasks[index].completed;
-  res.json({ message: "Task updated" });
+  res.json({ message: "Updated" });
 });
 
-// START SERVER
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
